206171a943dfee33945c95764afbfbf4b7b9e5c7
virth <info@joshuawirth.ch>
2016-05-14 00:30:11 +0200

    router added

--- frontend/app/app.component.ts
+++ frontend/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { CardSelectionComponent } from './card-selection.component';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
 
 @Component({
   selector: 'business-card',
   templateUrl: 'app/app.html',
-  directives: [CardSelectionComponent]
+  directives: [CardSelectionComponent, ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS]
 })
+
+@RouteConfig([
+  {
+    path: '/cards',
+    name: 'Cards',
+    component: CardSelectionComponent
+  }
+])
 
 
 export class AppComponent { 
